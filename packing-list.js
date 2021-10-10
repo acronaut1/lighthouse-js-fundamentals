3d55e9b923d6abde834c43d9bba684b98fc6818d
Oskar Pruski <opruski@ryerson.ca>
2021-10-10 04:42:04 +0000

    added while method

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,14 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
+/* 'ForLoop' method..
 for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]); */ 
+
+// 'While' method..
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
+
